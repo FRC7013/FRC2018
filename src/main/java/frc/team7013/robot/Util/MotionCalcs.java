1dceab2491da2f42a50de8cb6d579a195229c688
gabecasciano <gabecasciano@gmail.com>
2018-03-02 16:06:19 -0500

    Setting it up for chris

--- src/main/java/frc/team7013/robot/Util/MotionCalcs.java
+++
@@ -1,7 +0,0 @@
-package frc.team7013.robot.Util;
-
-public class MotionCalcs {
-
-    public static double setpointCalc(double reduction, int pulse_per_revolution, double setpoint){ return (reduction*pulse_per_revolution*setpoint)/360; }
-    public static double armExtensionCalc(double arm_length, )
-}
