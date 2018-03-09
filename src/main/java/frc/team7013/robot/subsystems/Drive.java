19986437efbdcd3c93278678a67b7826dae49808
Chris L <christopher.lansdale@gmail.com>
2018-03-09 16:50:25 -0500

    Saving latest changes to drive class. Need to get auto commands working.

--- src/main/java/frc/team7013/robot/subsystems/Drive.java
+++ src/main/java/frc/team7013/robot/subsystems/Drive.java
@@ -2,6 +2,7 @@ package frc.team7013.robot.subsystems;
 
 import frc.team7013.robot.TPackage.loop.Loop;
 import frc.team7013.robot.TPackage.loop.Looper;
+import frc.team7013.robot.TPackage.pid.TSpeedPID;
 import frc.team7013.robot.TPackage.sensors.encoder.TPwmEncoder;
 import frc.team7013.robot.TPackage.sensors.gyro.TAnalogGyro;
 import frc.team7013.robot.TPackage.speedcontroller.TPwmSpeedController;
@@ -20,7 +21,7 @@ public class Drive extends Subsystem {
     //The control states of the drive system
     public enum DriveControlState {
         OPEN_LOOP,  //open loop control
-        VELOCITY_SETPOINT   //velocity PID control
+        VELOCITY_SETPOINT,   //velocity PID control
     }
 
     //Control states
@@ -31,12 +32,16 @@ public class Drive extends Subsystem {
     private final TPwmEncoder mLeftEncoder, mRightEncoder;
     private final TAnalogGyro mGyro;
 
+    //PID
+    private final TSpeedPID leftSpeedPid;
+    private final TSpeedPID rightSpeedPid;
+    boolean speedPidsActive = false;
+
     private final Loop mLoop = new Loop() {
         @Override
         public void onStart(double timestamp) {
             synchronized (Drive.this) {
                 setOpenLoop(0,0);
-                setVelocitySetpoint(0,0);
                 mGyro.reset();
             }
         }
@@ -44,16 +49,7 @@ public class Drive extends Subsystem {
         @Override
         public void onLoop(double timestamp) {
             synchronized (Drive.this) {
-                switch(mDriveControlState) {
-                    case OPEN_LOOP: //call setOpenLoop(leftSpeed, rightSpeed) IN TELEOP_PERIODIC
-                        return;
-                    case VELOCITY_SETPOINT:
-                        DO SHIT
-                        break;
-                        default:
-                            System.out.println("ERROR: Unexpected drive control state: " + mDriveControlState);
-                            break;
-                }
+
             }
         }
 
@@ -71,7 +67,32 @@ public class Drive extends Subsystem {
         mLeftDrive = new TPwmSpeedController(TPwmSpeedControllerType.SPARK, RobotMap.LEFT_DRIVE_MOTOR_PORT, RobotConst.LEFT_MOTOR_ORIENTATION);
         mRightDrive = new TPwmSpeedController(TPwmSpeedControllerType.SPARK,RobotMap.RIGHT_DRIVE_MOTOR_PORT,RobotConst.RIGHT_MOTOR_ORIENTATION);
 
+        mGyro.setSensitivity(0.0016133333333333334);
+
+        leftSpeedPid = new TSpeedPID(RobotConst.DRIVE_SPEED_PID_KP);
+        rightSpeedPid = new TSpeedPID(RobotConst.DRIVE_SPEED_PID_KP);
+        disableSpeedPids();
+
         setOpenLoop(0,0);
+    }
+
+    private void disableSpeedPids() {
+        if(speedPidsActive) {
+            leftSpeedPid.disable();
+            rightSpeedPid.disable();
+            speedPidsActive = false;
+        }
+    }
+
+    private void enableSpeedPids() {
+        if(mLeftEncoder == null || mRightEncoder == null) { //If there are no encoders, don't enable pids
+            return;
+        }
+        if(!speedPidsActive) {
+            leftSpeedPid.enable();
+            rightSpeedPid.enable();
+            speedPidsActive = true;
+        }
     }
 
     @Override
@@ -82,7 +103,8 @@ public class Drive extends Subsystem {
     //Set up open loop control
     public synchronized void setOpenLoop(double leftSignal, double rightSignal) {
         if(mDriveControlState != DriveControlState.OPEN_LOOP) {
-            SET UP OPEN LOOP;
+            speedPidsActive = false;
+            disableSpeedPids();
             mDriveControlState = DriveControlState.OPEN_LOOP;
         }
         mLeftDrive.set(leftSignal);
@@ -110,22 +132,25 @@ public class Drive extends Subsystem {
         mGyro.reset();
     }
 
-    public synchronized void setVelocitySetpoint(double leftSpeed, double rightSpeed) {
+    public synchronized void setVelocitySetpoint(double leftSpeed, double rightSpeed) { //Needs to be called at 200Hz, NOT IN THIS CLASS
         if(mDriveControlState != DriveControlState.VELOCITY_SETPOINT) {
-            SET UP VELOCITY SETPOINT;
+            speedPidsActive = true;
+            enableSpeedPids();
             mDriveControlState = DriveControlState.VELOCITY_SETPOINT;
         }
         updateVelocitySetpoint(leftSpeed, rightSpeed);
     }
 
     private synchronized void updateVelocitySetpoint(double leftSpeed, double rightSpeed) {
-        if(mDriveControlState != DriveControlState.VELOCITY_SETPOINT) {
-            DO SPEED PID SHIT;
-        } else {
-            System.out.println("Hit a bad velocity control state");
-            mLeftDrive.set(0);
-            mRightDrive.set(0);
-        }
+        leftSpeedPid.setSetpoint(leftSpeed);
+        rightSpeedPid.setSetpoint(rightSpeed);
+
+        leftSpeedPid.calculate(getLeftVelocityUnitless() / RobotConst.MAX_DRIVE_SPEED);
+        rightSpeedPid.calculate(getRightVelocityUnitless() / RobotConst.MAX_DRIVE_SPEED);
+
+        mLeftDrive.set(leftSpeedPid.get());
+        mRightDrive.set(rightSpeedPid.get());
+
     }
 
     public double getLeftDistanceInches() {
@@ -146,6 +171,10 @@ public class Drive extends Subsystem {
 
     public double getRightVelocityUnitless() {
         return mRightEncoder.getRate();
+    }
+
+    public double getLeftRightAverageVelocityUnitless() {
+        return (mLeftEncoder.getRate() + mRightEncoder.getRate())/2;
     }
 
 }
